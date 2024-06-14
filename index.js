2f9f2057d2ec061fe1fef7ea31399264cb0af124
faridkotia <v.vansh@iitg.ac.in>
2024-06-14 13:56:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Add your JavaScript code here if needed for interactivity
+// For example, you could add a smooth scroll effect for the navigation links
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
